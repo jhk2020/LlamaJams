0249ded5c8694a295c27019cdea8b94357e141c7
Hoon <jhk2020@gmail.com>
2016-02-07 18:19:25 -0800

    [Fix] Small fix for mongo uri

--- src/server/db/config.js
+++ src/server/db/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   secret: 'llamjamsllamajams',
-  database: 'MONGOLAB_URI=mongodb://<dbuser>:<dbpassword>@ds059125.mongolab.com:59125/heroku_92xwhzpd'
+  database: 'mongodb://<dbuser>:<dbpassword>@ds059125.mongolab.com:59125/heroku_92xwhzpd'
 }
